b270587be87f26d1a305aa6bb8e9cbff7c6829df
Hasan <hasan.bilgin@ozu.edu.tr>
2020-12-06 22:28:02 +0300

    fixed errors related to login

--- sign_up_login_page/sign_up_login.js
+++ sign_up_login_page/sign_up_login.js
@@ -1,21 +1,28 @@
 function signUp() {
-  var email = document.getElementsByName("email_address_sign_up");
-  var password = document.getElementsByName("password_sign_up");
-  if (localStorage) localStorage.setItem(email, password);
-  else alert("Sorry, your browser does not support local storage");
+  var email = document.getElementsByName("email_address_sign_up")[0].value;
+  var password = document.getElementsByName("password_sign_up")[0].value;
+  if (localStorage) {
+	if (localStorage.getItem(email) == null) {
+		localStorage.setItem(email, password);
+	} else {
+		document.getElementById("sign_up_error").innerHTML = "user already exists";
+	  }
+  } else alert("Sorry, your browser does not support local storage");
 }
 
 function login() {
-  var email = document.getElementsByName("email_address_login");
-  var password = document.getElementsByName("password_login");
+  var email = document.getElementsByName("email_address_login")[0].value;
+  var password = document.getElementsByName("password_login")[0].value;
   if (localStorage.getItem(email) == null) {
     document.getElementById("login_error").innerHTML = "user does not exist";
+	  return false;
   }
   else if (localStorage.getItem(email) == password) {
+	  sessionStorage.setItem("login", true);
     return true;
   }
   else {
+	  document.getElementById("login_error").innerHTML = "wrong password";
     return false;
-    document.getElementById("login_error").innerHTML = "wrong password";
   }
 }
