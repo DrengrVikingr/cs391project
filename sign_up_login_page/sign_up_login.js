cc3a45aa09bb5911b6c4c949a85380568c233571
Hasan <hasan.bilgin@ozu.edu.tr>
2020-12-12 00:41:14 +0300

    fixed sign up bug and added the 1st version of css

--- sign_up_login_page/sign_up_login.js
+++ sign_up_login_page/sign_up_login.js
@@ -1,28 +1,49 @@
+function checkEmpty() {
+  var email = document.getElementsByName("email_address_sign_up")[0].value;
+  var name = document.getElementsByName("name")[0];
+  var surname = document.getElementsByName("surname")[0];
+  var birthdate = document.getElementById("birthdate").value;
+  var password = document.getElementsByName("password_sign_up")[0].value;
+  return email != "" | name != null | surname != null | birthdate != "" | password != "";
+}
+
 function signUp() {
   var email = document.getElementsByName("email_address_sign_up")[0].value;
   var password = document.getElementsByName("password_sign_up")[0].value;
   if (localStorage) {
-	if (localStorage.getItem(email) == null) {
-		localStorage.setItem(email, password);
-	} else {
-		document.getElementById("sign_up_error").innerHTML = "user already exists";
-	  }
-  } else alert("Sorry, your browser does not support local storage");
+	  if (localStorage.getItem(email) == null & checkEmpty()) {
+      localStorage.setItem(email, password);
+      return true;
+    } else if (!checkEmpty()) {
+      document.getElementById("sign_up_error").innerHTML = "please fill in each field";
+      return false;
+	  } else {
+      document.getElementById("sign_up_error").innerHTML = "user already exists";
+      return false;
+	    }
+  } else {
+    alert("Sorry, your browser does not support local storage");
+    return false;
+  }
 }
 
 function login() {
   var email = document.getElementsByName("email_address_login")[0].value;
   var password = document.getElementsByName("password_login")[0].value;
-  if (localStorage.getItem(email) == null) {
-    document.getElementById("login_error").innerHTML = "user does not exist";
-	  return false;
-  }
-  else if (localStorage.getItem(email) == password) {
-	  sessionStorage.setItem("login", true);
-    return true;
-  }
-  else {
-	  document.getElementById("login_error").innerHTML = "wrong password";
-    return false;
+  if (localStorage) {
+    if (localStorage.getItem(email) == null) {
+      document.getElementById("login_error").innerHTML = "user does not exist";
+      return false;
+    }
+    else if (localStorage.getItem(email) == password) {
+      sessionStorage.setItem("login", true);
+      return true;
+    }
+    else {
+      document.getElementById("login_error").innerHTML = "wrong password";
+      return false;
+    }
+  } else {
+    alert("Sorry, your browser does not support local storage");
   }
 }
